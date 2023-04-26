7c6fc01ec6cef0b6d409d2cdf27efb93d4a9f677
mpadil18 <mpadil18@ucsc.edu>
2023-04-26 12:05:19 -0700

    Cleaned up UI and added speech bubble

--- frontend/src/Create-Goal/Create-Goal.js
+++ frontend/src/Create-Goal/Create-Goal.js
@@ -1,20 +1,20 @@
 import "./Create-Goal.css"
-
+import ProfText from "../assets/ProfTextA.svg"
 function CreateGoal() {
     return (
-      <div className="Create-Goal">
-        Create Goal
-        <textarea className="GoalText" rows="5" cols="33"
-        placeholder="Example: Brush Teeth, Floss, Manage Addiction, Drink Water.."
-        />
+      <div className="CreateGoal">
+        <div className="CreateGoalBubble">
+          <p className="BubbleHeader">Create Goal</p>
+          <textarea className="GoalText" rows="5" cols="33"
+          placeholder="Example: Brush Teeth, Floss, Manage Addiction, Drink Water.."
+          />
 
-        <button> 
-          Save Goal
-        </button>
-
-        <div className = "text-box"> 
-          Any goal is a good goal, no matter how small !
+          <button className="BubbleButton"> 
+            Save Goal
+          </button>
         </div>
+
+        <img className = "ProfessorText" src={ProfText} alt="Professor speech bubble"></img>
 
       </div>
     );
