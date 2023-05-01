a2a837d4595d4ba39b41f603c1b775736fbe602c
ajmallon <59618383+ajmallon@users.noreply.github.com>
2023-05-01 10:53:27 -0700

    Login page (#5)
    * initial login page commit
    
    * edited login page
    
    * logo images
    
    * added login buttons
    
    * cleaned up login page styling + restructed assets
    
    * added routing functionality and set up connection for firebase implementation
    
    * added sign in functionality
    
    * fixed textbox
    
    * made changes as outlined by maritza
    
    * changed it as per maritza's outline frfr ong
    
    * added dummy registration
    
    * package json
    
    ---------
    
    Co-authored-by: Katherine <kaabeckl@ucsc.edu>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Login/Login"
+import RegPage from "./Registration/RegPage";
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 
+
+//add additional paths to different pages using same syntax <Route path = "..." element = {<.../>} />
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "" element = {<Login/>} /> 
+        <Route path = "/" element = {<Login/>} />
+        <Route path = "/RegPage" element = {<RegPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
