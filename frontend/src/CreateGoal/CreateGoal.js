4bb78af9dea58dfa57c8857422eac15f51aa7bf1
Ojal M <ojalmaps@gmail.com>
2023-05-02 11:21:22 -0700

    Adding state for saving the user text

--- frontend/src/CreateGoal/CreateGoal.js
+++ frontend/src/CreateGoal/CreateGoal.js
@@ -1,15 +1,26 @@
 import "./CreateGoal.css"
 import ProfText from "../assets/ProfTextA.svg"
+import React, { useState } from "react"
+
 function CreateGoal() {
+
+  const [goalText, setGoalText] = useState("");
+  
+  const logState = () => {
+    console.log(goalText); 
+  }
+
     return (
       <div className="CreateGoal">
         <div className="CreateGoalBubble">
           <p className="BubbleHeader">Create Goal</p>
           <textarea className="GoalText" rows="5" cols="33"
           placeholder="Example: Brush Teeth, Floss, Manage Addiction, Drink Water.."
+          value={goalText}
+          onChange = {(e) => setGoalText(e.target.value)}
           />
 
-          <button className="BubbleButton"> 
+          <button className="BubbleButton" onClick = {logState}> 
             Save Goal
           </button>
         </div>
