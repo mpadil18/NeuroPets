1fdb2410ff315947013de4dc0202d9af1ad8d194
Joseph Scheidt <130117620+Toa3t@users.noreply.github.com>
2023-05-07 14:32:28 -0700

    Adapting RegPage to new CSS + Firebase functionality (#10)
    *Updating to follow the current css guidelines
    *Adding connection to firebase +error handling

--- frontend/src/Registration/RegPage.js
+++ frontend/src/Registration/RegPage.js
@@ -4,29 +4,89 @@ import TextBox from "./textBox";
 import TopText from "../assets/upperBubble.svg"
 import { useNavigate } from "react-router-dom"
 import BottomText from "../assets/lowerBubble.svg"
+import { useState } from 'react';
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {createUserDb} from '../Backend/handleSubmit';
 
 function RegPage() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState(''); // defines username state variable and assogns the function setUsername to be handled when 'handleUsernameChange' is triggered
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
 
-  const createGoal = (e) => {
-    navigate('../createGoal');
+  // these are event handlers that trigger when any of the input fields are modified, i.e. a user typing in their information
+  // called due to on change modifiers to the textbox divs below
+  const handleUsernameChange = (value) => {
+    setUsername(value);
   }
 
-    return (
-      <div className="Registration">
-        <div className="Imgbox">
-          <img className = "Bubble" src={TopText} alt="top speech bubble"></img>
-          <img className = "Bubble" src={BottomText} alt="bot speech bubble"></img>
-        </div>
-        <div className="InputBubble">
-          <p className="SmallBubbleHeader">Create Account</p>
-            <TextBox placeholder="Username" />
-            <TextBox placeholder="Password" />
-            <TextBox placeholder="Confirm Password" />
-          <button className="bubbleButton"
-          onClick = {createGoal}  
-          >Register</button> 
-        </div>
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  }
+
+  const handleConfirmPasswordChange = (value) => {
+    setConfirmPassword(value);
+  }
+
+
+  const createGoal = (e) => {
+    if (username.length === 0|| password.length === 0 ||confirmPassword.length === 0 ){
+      setErrorMsg("Empty field detected")
+    }
+    else if (password !== confirmPassword ) {
+      setErrorMsg("Passwords not matching ")
+    }
+    else if (confirmPassword.length < 5){
+      setErrorMsg("Passwords not matching ")
+    } else {
+      // create new user account with Firebase Authentication
+      createUserWithEmailAndPassword(auth, username, password)
+        .then((userCredential) => {
+          var user = userCredential.user;
+          createUserDb(user.uid, user.email)
+
+          navigate('../CreateGoal');
+
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          console.log(errorCode)
+          
+          if (errorCode === 'auth/email-already-in-use') {
+            setErrorMsg("Email already in use");
+          } 
+          else if(errorCode === 'auth/invalid-email') {
+            setErrorMsg("Enter a valid email address");
+          }
+          else if (errorCode === 'auth/weak-password'){
+            setErrorMsg("Enter a stronger password");
+          }
+          else{
+            setErrorMsg("Please refresh your page");
+          }
+         
+        });
+    }
+  }
+
+  return (
+    <div className="Registration">
+      <div className="Imgbox">
+        <img className = "Bubble" src={TopText} alt="top speech bubble"></img>
+        <img className = "Bubble" src={BottomText} alt="bot speech bubble"></img>
+      </div>
+      <div className="InputBubble">
+        <p className="SmallBubbleHeader">Create Account</p>
+          <TextBox placeholder="Email" onChange={handleUsernameChange} />
+          <TextBox placeholder="Password" onChange={handlePasswordChange} />
+          <TextBox placeholder="Confirm Password" onChange={handleConfirmPasswordChange} />
+          {errorMsg && <p> Error: {errorMsg}</p>}
+        <button className="bubbleButton"
+        onClick = {createGoal}  
+        >Register</button> 
+      </div>
       </div>
     );
 }
