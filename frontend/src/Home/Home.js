2db8a93fcde6381adc7acd18b2d7531cd56109e5
Katherine <kaabeckl@ucsc.edu>
2023-05-06 17:06:59 -0700

    added more navbar icons, added prof text bubble, centered content

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,14 +1,18 @@
 import "./Home.css"
-import pet from "../assets/pet.svg"
+import ProfText from "../assets/ProfTextB.svg"
+import Pet from "../assets/pet.svg"
 
 function Home() {
     return (
         <div className = "Home">
-            <img className = "pet" src = {pet} alt = "sample neuropet"/>
+            <img className = "pet" src = {Pet} alt = "sample neuropet"/>
+            <img className = "ProfessorText" src={ProfText} alt="Professor speech bubble"></img>
             <nav className = "navbar">
                 <ul className = "navlist">
                     <li className = "editGoalIcon"/>
+                    <li className = "petHabitatIcon"/>
                     <li className = "homeIcon"/>
+                    <li className = "shopIcon"/>
                     <li className = "settingsIcon"/>
                 </ul>
             </nav>
