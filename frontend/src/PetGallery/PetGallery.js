33e385d558cf56005d802a66d4ad311c3ed29fcb
Ojal M <ojalmaps@gmail.com>
2023-05-15 22:31:36 -0700

    Need to use png instead of jpg for background transperancy

--- frontend/src/PetGallery/PetGallery.js
+++ frontend/src/PetGallery/PetGallery.js
@@ -1,19 +1,20 @@
-
-import bunny0svg from "../assets/sprites/bunny0.svg";
-import bunny1svg from "../assets/sprites/bunny1.svg";
 import bunny0 from "../assets/sprites/bunny0.jpg";
 import bunny1 from "../assets/sprites/bunny1.jpg";
 import bunny2 from "../assets/sprites/bunny2.jpg";
 
+import penguin1 from "../assets/sprites/penguin1.png"
+import penguin2 from "../assets/sprites/penguin2.png"
+import penguin3 from "../assets/sprites/penguin3.png"
 function PetGallery() {
 
     return (
-     <div>
-          <img className = "pet" src = {bunny0svg} alt= "bunny0" />
-          <img className = "pet" src = {bunny1svg} alt= "bunny1svg"/>
+     <div style={{backgroundColor: 'pink'}}>
           <img className = "pet" src = {bunny0} alt= "stage 0"/>
           <img className = "pet" src = {bunny1} alt= "stage 1"/>
           <img className = "pet" src = {bunny2} alt= "stage 2"/>
+          <img className = "pet" src = {penguin1} alt= "stage 1"/>
+          <img className = "pet" src = {penguin2} alt= "stage 2"/>
+          <img className = "pet" src = {penguin3} alt= "stage 3"/>
     </div>
     );
 }
