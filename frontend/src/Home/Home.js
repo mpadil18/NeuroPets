1f5545ce9e67a522bc028738f4c0c416348ad650
Ojal M <ojalmaps@gmail.com>
2023-05-18 18:37:20 -0700

    Renaming goals to GoalArray

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -22,12 +22,12 @@ function Home() {
              const docSnap = await getDoc(docRef);
              if (docSnap.exists()) {
             
-                 var goals = docSnap.data().goal;
+                 var goalArray = docSnap.data().goal;
                  let goalIndex = docSnap.data().activeGoal;
-                 let progressCount = goals[goalIndex].progressCounter + 1;
-                 goals[goalIndex].progressCounter = progressCount;
+                 let progressCount = goalArray[goalIndex].progressCounter + 1;
+                 goalArray[goalIndex].progressCounter = progressCount;
                  await updateDoc(docRef, {
-                     goal : goals
+                     goal : goalArray
                  });
             }
         }      
@@ -36,7 +36,6 @@ function Home() {
     const completeGoal = (e) => {
         setGoalComplete(true);
         setProgressCount(progressCounter + 1);
-        
         updateCount();
     }
 
