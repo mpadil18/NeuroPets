a3ed825c3148df818cbede224bd4903f1f0914b3
Adam Mallon <ajmallon@ucsc.edu>
2023-05-21 10:45:40 -0700

    reset user lastProgressMade timestamp when goal is deleted

--- frontend/src/DeleteGoal/DeleteGoal.js
+++ frontend/src/DeleteGoal/DeleteGoal.js
@@ -4,6 +4,7 @@ import { getDoc, updateDoc, doc} from "firebase/firestore";
 import { db } from "../Backend/firebaseSetup.js";
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore';
+import { updateUserInfo } from "../Backend/handleSubmit";
 
 function DeleteGoal(props) {
     let user = props.user;
@@ -22,9 +23,10 @@ function DeleteGoal(props) {
                     const docSnap = await getDoc(docRef);
                     let goalArray = docSnap.data().goalArray;
                     let currGoal = goalArray[goalArray.length - 1];
-            
+                    let timeStamp = docSnap.data().lastProgressMade;
                     await updateDoc(docRef, {
-                        "goalArray": firebase.firestore.FieldValue.arrayRemove(currGoal)
+                        "goalArray": firebase.firestore.FieldValue.arrayRemove(currGoal),
+                        "lastProgressMade" : firebase.firestore.FieldValue.delete(timeStamp)
                     });
                 }
             }
