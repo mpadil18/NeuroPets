785bd68684a4774064dbe1279ad900a54041bfe7
Andrew Sanchez <annisanc@ucsc.edu>
2023-05-20 23:26:33 -0700

    presets

--- frontend/src/CreateGoal/CreateGoal.js
+++ frontend/src/CreateGoal/CreateGoal.js
@@ -10,6 +10,8 @@ function CreateGoal() {
 
   const [goalText, setGoalText] = useState("");
   const [errorMsg, setErrorMsg] = useState('');
+  const [presetGoals] = useState(["Eat Healthy", "Be Active", "Improve Mental Health", "Focus on Relationships", 
+  "Dedicate Time to a Hobby", "Learn an Instrument", "Maintain a Tidied Space"])
 
   const navigate = useNavigate();
 
@@ -51,9 +53,15 @@ function CreateGoal() {
       onUpdateNavigate(goalTuple);
     }
   }
+
+  const setPresetFunc = (id) => {
+    console.log(presetGoals[Number(id)])
+    setGoalText(presetGoals[Number(id)]);
+  }
   
   const submithandler = (e) => {
     e.preventDefault()
+    console.log("I GOT CALLED");
     
     if (goalText.length === 0){
       setErrorMsg("Please fill in a goal");
@@ -61,19 +69,73 @@ function CreateGoal() {
       updateGoal();
     }
   }
+
+
+  const accordionClick = (accId) => {
+    if (accId === "customGoal") {
+      document.getElementById("presetGoalDrop").style.display = "none";
+      if (document.getElementById("customGoalDrop").style.display === "inline-block") {
+        document.getElementById("customGoalDrop").style.display = "none";
+      }
+      else {
+        document.getElementById("customGoalDrop").style.display = "inline-block";
+      }
+    } 
+    else {
+      document.getElementById("customGoalDrop").style.display = "none";
+      if (document.getElementById("presetGoalDrop").style.display === "inline-block") {
+        document.getElementById("presetGoalDrop").style.display = "none";
+      }
+      else {
+        document.getElementById("presetGoalDrop").style.display = "inline-block";
+      }
+    }
+    setGoalText("");
+} 
     return (
       <div className="CreateGoal">
         <div className="InputBubble">
           <p className="BubbleHeader">Create Goal</p>
-          <textarea className="bubbleField" rows="5" cols="33"
+
+          <button id="customGoal" className="bubbleButton accordion" onClick = {(e) => accordionClick(e.target.id)}> 
+            Custom Goal
+          </button>
+          <textarea id="customGoalDrop" className="bubbleField" rows="5" cols="33"
           placeholder="Example: Brush Teeth, Floss, Manage Addiction, Drink Water.."
           value={goalText}
           onChange = {(e) => setGoalText(e.target.value)}
           />
 
+          <button id="presetGoal" className="bubbleButton accordion" onClick = {(e) => accordionClick(e.target.id)}> 
+            Preset Goal
+          </button>
+          <div className="presets" id="presetGoalDrop">
+            <button id="0" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Eat Healthy
+            </button>
+            <button id="1" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Be Active
+            </button>
+            <button id="2" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Improve Mental Health
+            </button>
+            <button id="3" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Focus on Relationships
+            </button>
+            <button id="4" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Dedicate Time to a Hobby
+            </button>
+            <button id="5" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Learn an Instrument
+            </button>
+            <button id="6" className="bubbleButton" onClick = {(e) => setPresetFunc(e.target.id)}> 
+            Maintain a Tidied Space
+            </button>
+          </div>
+
           {errorMsg && <p style={{margin: 0}}> Error: {errorMsg}</p>}
 
-          <button className="bubbleButton" onClick = {submithandler}> 
+          <button className="bubbleButton submission" onClick = {submithandler}> 
             Save Goal
           </button>
         </div>
