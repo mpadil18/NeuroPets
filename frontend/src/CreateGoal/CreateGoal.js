7374394e90276b8250572d5fab05d6ebb7037f27
Ojal M <ojalmaps@gmail.com>
2023-05-30 23:44:01 -0700

    Edit the handleSubmit + creategoal:

--- frontend/src/CreateGoal/CreateGoal.js
+++ frontend/src/CreateGoal/CreateGoal.js
@@ -3,11 +3,10 @@ import React, { useState } from "react"
 import { auth } from "../Backend/firebaseSetup";
 import { useNavigate } from "react-router-dom"
 import ProfText from "../assets/branding/ProfTextA.svg"; 
-import { createNewGoal } from "../Backend/handleSubmit";
-import PetContract from "./petContract";
+import { useHistory } from 'react-router-dom';
 
 function CreateGoal() {
-
+  const history = useHistory();
   const [goalText, setGoalText] = useState("");
   const [errorMsg, setErrorMsg] = useState('');
   const [presetGoals] = useState(["Eat Healthier", "Be More Active", "Improve Mental Health", "Focus on Relationships", 
@@ -15,26 +14,6 @@ function CreateGoal() {
 
   const navigate = useNavigate();
   const user = auth.currentUser;
-
-
-  // This code probably doesn't work well on mobile...
-  // const onUpdateNavigate = async (newData) => {
-  //     // Retrieve most recent goal to error check.
-  //     // If update properly made, navigate user to Home
-  //     const user = auth.currentUser;
-  //     const docSnap = await getUserInfo(user.uid);
-  //     if (docSnap) {
-  //       const latestGoal = (docSnap.goalArray)[(docSnap.goalArray).length - 1];
-
-  //         if (latestGoal.goal === newData.goal && latestGoal.pet === newData.pet) {
-  //           navigate('../Home');
-  //         }
-  //         else {
-  //           console.log("ERROR- not updated properly")
-  //         }
-  //     }
-  // }
-
 
   const setPresetFunc = (id) => {
     // Function used for all preset goals 
@@ -53,11 +32,7 @@ function CreateGoal() {
     if (goalText.length === 0){
       setErrorMsg("Please fill in a goal");
     } else {
-     // createNewGoal(user.uid, goalText); // Call this function in the petContract instead
-      setTimeout(function(){
-        navigate('../Home');
-      }, 500);
-      
+      history.push('/petContract', { goalText: goalText });
     }
   }
 
--- frontend/src/CreateGoal/petContract.js
+++ frontend/src/CreateGoal/petContract.js
@@ -1,18 +1,39 @@
 
 import React from "react"
-import { useNavigate } from "react-router-dom"
-import { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../Backend/firebaseSetup";
+import { createNewGoal } from "../Backend/handleSubmit";
 // Props that need to be passed is goalText
 
-function PetContract(goalText){
+function PetContract(props){
 
-    const goalText = goalText; 
+    const user = auth.currentUser;
+    const goalText = props.location.state; 
     const [petName, setPetName] = useState(null);
+
+    console.log(goalText);
+
+
+    const submithandler = (e) => {
+      e.preventDefault()
+      
+      if (goalText.length === 0){
+        setErrorMsg("Please fill in a goal");
+      } else {
+        createNewGoal(user.uid, goalText, petName); // Call this function in the petContract instead
+        setTimeout(function(){
+          navigate('../Home');
+        }, 500);
+        
+      }
+    }
+
 
     return(
       <div className="PetContract">
 
+        <div>
         NeuroPet Adoption Form & Contract to Myself
         
         I <input placeholder="Name"></input> vow to look after my NeuroPet
@@ -24,6 +45,11 @@ function PetContract(goalText){
 
         I will change, and achieve my goal, by signing up for it one day at a time, in a row.
 
+        </div>
+
+        <button onClick = {submithandler}> 
+            Sign here 
+        </button>
 
       </div>  
     )
